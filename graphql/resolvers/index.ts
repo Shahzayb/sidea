45bb3b57a318982fb42268d7727434246dda9706
Shahzaib Sarwar <imshahzayb@gmail.com>
2020-06-06 13:15:27 +0500

    added auth related queries & mutations & resolvers

--- graphql/resolvers/index.ts
+++ graphql/resolvers/index.ts
@@ -1,9 +1,228 @@
+import { AuthenticationError, UserInputError } from 'apollo-server-micro';
+import validator from 'validator';
+import gravatar from 'gravatar';
+import bcrypt from 'bcryptjs';
+import { createJwtToken } from '../../utils/jwt';
 import { Resolvers } from '../types/index';
-import { Context } from '../../pages/api/graphql';
+import { User } from '@prisma/client';
 
-const resolvers: Resolvers<Context> = {
-  Query: {},
-  Mutation: {},
+const resolvers: Resolvers = {
+  Query: {
+    me(_, __, { user }) {
+      if (!user) {
+        throw new AuthenticationError('your are not logged in');
+      }
+      return user;
+    },
+    async user(_, { id }, { prisma }) {
+      if (
+        !validator.isInt(id.trim(), {
+          allow_leading_zeroes: true,
+        })
+      ) {
+        return null;
+      }
+
+      const userId = validator.toInt(id);
+      return prisma.user.findOne({
+        where: {
+          id: userId,
+        },
+      });
+    },
+  },
+  Mutation: {
+    async login(_, { input: { username, password } }, { prisma }) {
+      username = username.trim();
+      password = password.trim();
+      // a username cannot be an email address
+      let user: User | null = null;
+      if (validator.isEmail(username)) {
+        user = await prisma.user.findOne({
+          where: {
+            email: validator.normalizeEmail(username) as string,
+          },
+        });
+      } else {
+        user = await prisma.user.findOne({
+          where: {
+            username: username,
+          },
+        });
+      }
+      if (!user) {
+        throw new AuthenticationError('invalid username or password');
+      }
+
+      const isCorrectPassword = await bcrypt.compare(password, user.password);
+      if (!isCorrectPassword) {
+        throw new AuthenticationError('invalid username or password');
+      }
+
+      const token = createJwtToken({ id: user.id });
+
+      return {
+        token,
+        user,
+      };
+    },
+    async signup(_, { input }, { prisma }) {
+      const errors: { param: keyof typeof input; msg: string }[] = [];
+      // trim all input
+      input.email = input.email.trim();
+      input.name = input.name.trim();
+      input.username = input.username.trim();
+      input.password = input.password.trim();
+
+      // check if name is valid
+      if (!input.name) {
+        errors.push({
+          param: 'name',
+          msg: 'name is required',
+        });
+      }
+
+      // check if password is valid
+      if (!input.password) {
+        errors.push({
+          param: 'password',
+          msg: 'password is required',
+        });
+      } else if (input.password.length < 8) {
+        errors.push({
+          param: 'password',
+          msg: 'password is too short. should be at least 8 character',
+        });
+      } else {
+        // hash the password
+        input.password = await bcrypt.hash(input.password, 8);
+      }
+
+      // check if email is valid
+      if (!input.email) {
+        errors.push({
+          param: 'email',
+          msg: 'email is required',
+        });
+      } else if (!validator.isEmail(input.email)) {
+        errors.push({
+          param: 'email',
+          msg: 'email is invalid',
+        });
+      } else {
+        // normalize email & check if this email is unique
+        input.email = validator.normalizeEmail(input.email) as string;
+        const count = await prisma.user.count({
+          where: {
+            email: input.email,
+          },
+        });
+        if (count !== 0) {
+          errors.push({
+            param: 'email',
+            msg: 'email is already taken',
+          });
+        }
+      }
+
+      // check if username is valid
+      if (!input.username) {
+        errors.push({
+          param: 'username',
+          msg: 'username is required',
+        });
+      }
+      // check if username is not email
+      else if (validator.isEmail(input.username)) {
+        errors.push({
+          param: 'username',
+          msg: 'username cannot be an email address',
+        });
+      }
+      // check if username does not contains any spaces ^[0-9a-zA-Z]*$
+      else if (!validator.isAlphanumeric(input.username)) {
+        errors.push({
+          param: 'username',
+          msg: 'username can only be alpha numeric',
+        });
+      }
+      // check if username is unique
+      else {
+        const count = await prisma.user.count({
+          where: {
+            username: input.username,
+          },
+        });
+        if (count !== 0) {
+          errors.push({
+            param: 'username',
+            msg: 'username is already taken',
+          });
+        }
+      }
+
+      // check input length
+      if (
+        !validator.isLength(input.email, {
+          max: 255,
+        })
+      ) {
+        errors.push({
+          param: 'email',
+          msg: 'email is too long. max character limit is 255',
+        });
+      }
+      if (
+        !validator.isLength(input.name, {
+          max: 255,
+        })
+      ) {
+        errors.push({
+          param: 'name',
+          msg: 'name is too long. max character limit is 255',
+        });
+      }
+      if (
+        !validator.isLength(input.username, {
+          max: 255,
+        })
+      ) {
+        errors.push({
+          param: 'username',
+          msg: 'username is too long. max character limit is 255',
+        });
+      }
+
+      if (errors.length !== 0) {
+        throw new UserInputError('input is invalid', { errors });
+      }
+
+      // generate avatar
+      const avatar = gravatar.url(input.email);
+      // create user
+      const user = await prisma.user.create({
+        data: {
+          ...input,
+          avatar,
+        },
+      });
+      // create jwt token
+      const token = createJwtToken({ id: user.id });
+
+      return {
+        token,
+        user,
+      };
+    },
+  },
+  User: {
+    email(parent, _, { user }, info) {
+      if (!user || parent.id !== user.id) {
+        return null;
+      }
+      return parent.email;
+    },
+  },
 };
 
 export default resolvers;
--- graphql/types/index.ts
+++ graphql/types/index.ts
@@ -1,5 +1,8 @@
 import { GraphQLResolveInfo } from 'graphql';
+import { User as CustomUser } from '@prisma/client';
+import { Context } from '../../pages/api/graphql';
 export type Maybe<T> = T | null;
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 export type RequireFields<T, K extends keyof T> = { [X in Exclude<keyof T, K>]?: T[X] } & { [P in K]-?: NonNullable<T[P]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
@@ -12,23 +15,8 @@ export type Scalars = {
 
 export type Query = {
   __typename?: 'Query';
-  newIdeas: Array<Idea>;
-  topIdeas: Array<Idea>;
   me: User;
-  user: User;
-};
-
-
-export type QueryNewIdeasArgs = {
-  after_id?: Maybe<Scalars['ID']>;
-  limit: Scalars['Int'];
-};
-
-
-export type QueryTopIdeasArgs = {
-  interval: Interval;
-  after_id?: Maybe<Scalars['ID']>;
-  limit: Scalars['Int'];
+  user?: Maybe<User>;
 };
 
 
@@ -40,64 +28,16 @@ export type Mutation = {
   __typename?: 'Mutation';
   login: AuthResponse;
   signup: AuthResponse;
-  createIdea: Idea;
-  createFeature: Feature;
-  deleteIdea: Idea;
-  deleteFeature: Feature;
-  updateIdea: Idea;
-  updateFeature: Feature;
-  saveIdea: Save;
-  deleteSavedIdea: Save;
 };
 
 
 export type MutationLoginArgs = {
-  input?: Maybe<LoginInput>;
+  input: LoginInput;
 };
 
 
 export type MutationSignupArgs = {
-  input?: Maybe<SignupInput>;
-};
-
-
-export type MutationCreateIdeaArgs = {
-  input?: Maybe<CreateIdeaInput>;
-};
-
-
-export type MutationCreateFeatureArgs = {
-  input?: Maybe<CreateFeatureInput>;
-};
-
-
-export type MutationDeleteIdeaArgs = {
-  id: Scalars['ID'];
-};
-
-
-export type MutationDeleteFeatureArgs = {
-  id: Scalars['ID'];
-};
-
-
-export type MutationUpdateIdeaArgs = {
-  input?: Maybe<UpdateIdeaInput>;
-};
-
-
-export type MutationUpdateFeatureArgs = {
-  input?: Maybe<UpdateFeatureInput>;
-};
-
-
-export type MutationSaveIdeaArgs = {
-  id: Scalars['ID'];
-};
-
-
-export type MutationDeleteSavedIdeaArgs = {
-  id: Scalars['ID'];
+  input: SignupInput;
 };
 
 export type LoginInput = {
@@ -112,38 +52,6 @@ export type SignupInput = {
   password: Scalars['String'];
 };
 
-export type CreateFeatureInput = {
-  title: Scalars['String'];
-  body?: Maybe<Scalars['String']>;
-};
-
-export type CreateIdeaInput = {
-  title: Scalars['String'];
-  body: Scalars['String'];
-  user: User;
-  tags: Array<Scalars['String']>;
-  features: Array<CreateFeatureInput>;
-};
-
-export type UpdateIdeaInput = {
-  id: Scalars['ID'];
-  title?: Maybe<Scalars['String']>;
-  body?: Maybe<Scalars['String']>;
-};
-
-export type UpdateFeatureInput = {
-  id: Scalars['ID'];
-  title?: Maybe<Scalars['String']>;
-  body?: Maybe<Scalars['String']>;
-};
-
-export enum Interval {
-  Day = 'DAY',
-  Week = 'WEEK',
-  Year = 'YEAR',
-  AllTime = 'ALL_TIME'
-}
-
 export type AuthResponse = {
   __typename?: 'AuthResponse';
   token: Scalars['String'];
@@ -155,65 +63,13 @@ export type User = {
   id: Scalars['ID'];
   name: Scalars['String'];
   username: Scalars['String'];
-  email: Scalars['String'];
+  email?: Maybe<Scalars['String']>;
   avatar: Scalars['String'];
-  ideas: Array<Idea>;
-  savedIdeas: Array<Idea>;
   createdAt: Scalars['String'];
 };
 
-
-export type UserIdeasArgs = {
-  after_id?: Maybe<Scalars['ID']>;
-  limit: Scalars['Int'];
-};
-
-
-export type UserSavedIdeasArgs = {
-  after_id?: Maybe<Scalars['ID']>;
-  limit: Scalars['Int'];
-};
-
-export type Idea = {
-  __typename?: 'Idea';
-  id: Scalars['ID'];
-  title: Scalars['String'];
-  body: Scalars['String'];
-  user: User;
-  tags: Array<Scalars['String']>;
-  likes: Array<Like>;
-  likesCount: Scalars['Int'];
-  isLikedByMe: Scalars['Boolean'];
-  features: Array<Feature>;
-  createdAt: Scalars['String'];
-};
-
-export type Feature = {
-  __typename?: 'Feature';
-  id: Scalars['ID'];
-  title: Scalars['String'];
-  body?: Maybe<Scalars['String']>;
-  idea: Idea;
-  createdAt: Scalars['String'];
-};
-
-export type Like = {
-  __typename?: 'Like';
-  id: Scalars['ID'];
-  idea: Idea;
-  user: User;
-  createdAt: Scalars['String'];
-};
-
-export type Save = {
-  __typename?: 'Save';
-  id: Scalars['ID'];
-  user: User;
-  idea: Idea;
-  createdAt: Scalars['String'];
-};
-
-
+export type WithIndex<TObject> = TObject & Record<string, any>;
+export type ResolversObject<TObject> = WithIndex<TObject>;
 
 export type ResolverTypeWrapper<T> = Promise<T> | T;
 
@@ -290,142 +146,67 @@ export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs
 ) => TResult | Promise<TResult>;
 
 /** Mapping between all available schema types and the resolvers types */
-export type ResolversTypes = {
+export type ResolversTypes = ResolversObject<{
   String: ResolverTypeWrapper<Scalars['String']>;
   Boolean: ResolverTypeWrapper<Scalars['Boolean']>;
   Query: ResolverTypeWrapper<{}>;
   ID: ResolverTypeWrapper<Scalars['ID']>;
-  Int: ResolverTypeWrapper<Scalars['Int']>;
   Mutation: ResolverTypeWrapper<{}>;
   LoginInput: LoginInput;
   SignupInput: SignupInput;
-  CreateFeatureInput: CreateFeatureInput;
-  CreateIdeaInput: CreateIdeaInput;
-  UpdateIdeaInput: UpdateIdeaInput;
-  UpdateFeatureInput: UpdateFeatureInput;
-  INTERVAL: Interval;
-  AuthResponse: ResolverTypeWrapper<AuthResponse>;
-  User: ResolverTypeWrapper<User>;
-  Idea: ResolverTypeWrapper<Idea>;
-  Feature: ResolverTypeWrapper<Feature>;
-  Like: ResolverTypeWrapper<Like>;
-  Save: ResolverTypeWrapper<Save>;
-};
+  AuthResponse: ResolverTypeWrapper<Omit<AuthResponse, 'user'> & { user: ResolversTypes['User'] }>;
+  User: ResolverTypeWrapper<CustomUser>;
+}>;
 
 /** Mapping between all available schema types and the resolvers parents */
-export type ResolversParentTypes = {
+export type ResolversParentTypes = ResolversObject<{
   String: Scalars['String'];
   Boolean: Scalars['Boolean'];
   Query: {};
   ID: Scalars['ID'];
-  Int: Scalars['Int'];
   Mutation: {};
   LoginInput: LoginInput;
   SignupInput: SignupInput;
-  CreateFeatureInput: CreateFeatureInput;
-  CreateIdeaInput: CreateIdeaInput;
-  UpdateIdeaInput: UpdateIdeaInput;
-  UpdateFeatureInput: UpdateFeatureInput;
-  INTERVAL: Interval;
-  AuthResponse: AuthResponse;
-  User: User;
-  Idea: Idea;
-  Feature: Feature;
-  Like: Like;
-  Save: Save;
-};
+  AuthResponse: Omit<AuthResponse, 'user'> & { user: ResolversParentTypes['User'] };
+  User: CustomUser;
+}>;
 
-export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = {
-  newIdeas?: Resolver<Array<ResolversTypes['Idea']>, ParentType, ContextType, RequireFields<QueryNewIdeasArgs, 'limit'>>;
-  topIdeas?: Resolver<Array<ResolversTypes['Idea']>, ParentType, ContextType, RequireFields<QueryTopIdeasArgs, 'interval' | 'limit'>>;
+export type QueryResolvers<ContextType = Context, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = ResolversObject<{
   me?: Resolver<ResolversTypes['User'], ParentType, ContextType>;
-  user?: Resolver<ResolversTypes['User'], ParentType, ContextType, RequireFields<QueryUserArgs, 'id'>>;
-};
+  user?: Resolver<Maybe<ResolversTypes['User']>, ParentType, ContextType, RequireFields<QueryUserArgs, 'id'>>;
+}>;
 
-export type MutationResolvers<ContextType = any, ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']> = {
-  login?: Resolver<ResolversTypes['AuthResponse'], ParentType, ContextType, RequireFields<MutationLoginArgs, never>>;
-  signup?: Resolver<ResolversTypes['AuthResponse'], ParentType, ContextType, RequireFields<MutationSignupArgs, never>>;
-  createIdea?: Resolver<ResolversTypes['Idea'], ParentType, ContextType, RequireFields<MutationCreateIdeaArgs, never>>;
-  createFeature?: Resolver<ResolversTypes['Feature'], ParentType, ContextType, RequireFields<MutationCreateFeatureArgs, never>>;
-  deleteIdea?: Resolver<ResolversTypes['Idea'], ParentType, ContextType, RequireFields<MutationDeleteIdeaArgs, 'id'>>;
-  deleteFeature?: Resolver<ResolversTypes['Feature'], ParentType, ContextType, RequireFields<MutationDeleteFeatureArgs, 'id'>>;
-  updateIdea?: Resolver<ResolversTypes['Idea'], ParentType, ContextType, RequireFields<MutationUpdateIdeaArgs, never>>;
-  updateFeature?: Resolver<ResolversTypes['Feature'], ParentType, ContextType, RequireFields<MutationUpdateFeatureArgs, never>>;
-  saveIdea?: Resolver<ResolversTypes['Save'], ParentType, ContextType, RequireFields<MutationSaveIdeaArgs, 'id'>>;
-  deleteSavedIdea?: Resolver<ResolversTypes['Save'], ParentType, ContextType, RequireFields<MutationDeleteSavedIdeaArgs, 'id'>>;
-};
+export type MutationResolvers<ContextType = Context, ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']> = ResolversObject<{
+  login?: Resolver<ResolversTypes['AuthResponse'], ParentType, ContextType, RequireFields<MutationLoginArgs, 'input'>>;
+  signup?: Resolver<ResolversTypes['AuthResponse'], ParentType, ContextType, RequireFields<MutationSignupArgs, 'input'>>;
+}>;
 
-export type AuthResponseResolvers<ContextType = any, ParentType extends ResolversParentTypes['AuthResponse'] = ResolversParentTypes['AuthResponse']> = {
+export type AuthResponseResolvers<ContextType = Context, ParentType extends ResolversParentTypes['AuthResponse'] = ResolversParentTypes['AuthResponse']> = ResolversObject<{
   token?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   user?: Resolver<ResolversTypes['User'], ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType>;
-};
+}>;
 
-export type UserResolvers<ContextType = any, ParentType extends ResolversParentTypes['User'] = ResolversParentTypes['User']> = {
+export type UserResolvers<ContextType = Context, ParentType extends ResolversParentTypes['User'] = ResolversParentTypes['User']> = ResolversObject<{
   id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
   name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   username?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  email?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  email?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
   avatar?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  ideas?: Resolver<Array<ResolversTypes['Idea']>, ParentType, ContextType, RequireFields<UserIdeasArgs, 'limit'>>;
-  savedIdeas?: Resolver<Array<ResolversTypes['Idea']>, ParentType, ContextType, RequireFields<UserSavedIdeasArgs, 'limit'>>;
   createdAt?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType>;
-};
+}>;
 
-export type IdeaResolvers<ContextType = any, ParentType extends ResolversParentTypes['Idea'] = ResolversParentTypes['Idea']> = {
-  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
-  title?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  body?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  user?: Resolver<ResolversTypes['User'], ParentType, ContextType>;
-  tags?: Resolver<Array<ResolversTypes['String']>, ParentType, ContextType>;
-  likes?: Resolver<Array<ResolversTypes['Like']>, ParentType, ContextType>;
-  likesCount?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
-  isLikedByMe?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType>;
-  features?: Resolver<Array<ResolversTypes['Feature']>, ParentType, ContextType>;
-  createdAt?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  __isTypeOf?: IsTypeOfResolverFn<ParentType>;
-};
-
-export type FeatureResolvers<ContextType = any, ParentType extends ResolversParentTypes['Feature'] = ResolversParentTypes['Feature']> = {
-  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
-  title?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  body?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
-  idea?: Resolver<ResolversTypes['Idea'], ParentType, ContextType>;
-  createdAt?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  __isTypeOf?: IsTypeOfResolverFn<ParentType>;
-};
-
-export type LikeResolvers<ContextType = any, ParentType extends ResolversParentTypes['Like'] = ResolversParentTypes['Like']> = {
-  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
-  idea?: Resolver<ResolversTypes['Idea'], ParentType, ContextType>;
-  user?: Resolver<ResolversTypes['User'], ParentType, ContextType>;
-  createdAt?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  __isTypeOf?: IsTypeOfResolverFn<ParentType>;
-};
-
-export type SaveResolvers<ContextType = any, ParentType extends ResolversParentTypes['Save'] = ResolversParentTypes['Save']> = {
-  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
-  user?: Resolver<ResolversTypes['User'], ParentType, ContextType>;
-  idea?: Resolver<ResolversTypes['Idea'], ParentType, ContextType>;
-  createdAt?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  __isTypeOf?: IsTypeOfResolverFn<ParentType>;
-};
-
-export type Resolvers<ContextType = any> = {
+export type Resolvers<ContextType = Context> = ResolversObject<{
   Query?: QueryResolvers<ContextType>;
   Mutation?: MutationResolvers<ContextType>;
   AuthResponse?: AuthResponseResolvers<ContextType>;
   User?: UserResolvers<ContextType>;
-  Idea?: IdeaResolvers<ContextType>;
-  Feature?: FeatureResolvers<ContextType>;
-  Like?: LikeResolvers<ContextType>;
-  Save?: SaveResolvers<ContextType>;
-};
+}>;
 
 
 /**
  * @deprecated
  * Use "Resolvers" root object instead. If you wish to get "IResolvers", add "typesPrefix: I" to your config.
  */
-export type IResolvers<ContextType = any> = Resolvers<ContextType>;
+export type IResolvers<ContextType = Context> = Resolvers<ContextType>;
